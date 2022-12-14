f39ead430c4538c68c86d7bad68906d058eabfd3
Riku-Hatano <105307165+Riku-Hatano@users.noreply.github.com>
2022-12-13 16:06:18 -0800

    make change with dec12file

--- dec12/ency.php
+++ dec12/ency.php
@@ -1,3 +1,21 @@
 <?php
-    $asso = ["first" => "riku", "last" =>"hatano"];
+    $file = fopen("./contactlist.json","r");
+    $data = fread($file,filesize("./contactlist.json"));
+    fclose($file);
+    $key = "Hotdog";
+    $cipher_alog = "aes-256-gcm";
+    $ivlen = openssl_cipher_iv_length($cipher_alog); //get the length of the algorithm key
+    $iv = openssl_random_pseudo_bytes($ivlen); //use the length of the key to generate a pseudo random number with same length
+    $enc_text = openssl_encrypt($data,$cipher_alog,$key,0,$iv,$tag);
+    $ivdata = [$iv,$tag];
+    $file = fopen("./ivs.dat","w");
+    fwrite($file,implode(",",$ivdata));
+    fclose($file);
+    $file = fopen("./contactlist.json","w");
+    fwrite($file,$enc_text);
+    fclose($file);
+    echo "<p>$enc_text</p>";
+    $dec_text = openssl_decrypt($enc_text,$cipher_alog,$key,0,$iv,$tag);
+    echo "<p>$tag</p>";
+    echo "<p>$dec_text</p>";
 ?>
